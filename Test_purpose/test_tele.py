54e20ad92aa0019db5305214df59ff9167e07729
DeepWater1013 <Grasshopper1013@gmail.com>
2022-04-29 22:18:10 +0100

    precomplete

--- Test_purpose/test_tele.py
+++ Test_purpose/test_tele.py
@@ -1,7 +1,10 @@
-import telegram
-global bot
-global TOKEN
+# import telegram
+# global bot
+# global TOKEN
 
-bot = telegram.Bot(token='5132523958:AAH81rTLyDa2Xm_1ifEbA_qzFF8shk5oYL0')
+# bot = telegram.Bot(token='5132523958:AAH81rTLyDa2Xm_1ifEbA_qzFF8shk5oYL0')
 
-bot.sendMessage(chat_id='1863767894 ', text='‼️ CONFIGURAR ROBÔ 🤖 ')
+# bot.sendMessage(chat_id='1863767894 ', text='‼️ CONFIGURAR ROBÔ 🤖 ')
+roul_title = '\x1b[91m3  \x1b[0m\x1b[91m3  \x1b[0m\x1b[91m3  \x1b[0m\x1b[91m7  \x1b[0m\x1b[91m9  \x1b[0m\x1b[92m0  \x1b[0m2  4  6  8  10  '
+
+print(roul_title)
--- setup.py
+++ setup.py
@@ -5,3 +5,12 @@ except ImportError:
     from distutils.core import setup
     from distutils.extension import Extension
 
+from Cython.Distutils import build_ext
+
+ext_modules = [Extension("Autobet", ["Autobet.pyx"]), Extension(
+    "bet365_browser", ["bet365_browser.pyx"])]
+
+setup(
+    name='Generic model class',
+    cmdclass={'build_ext': build_ext},
+    ext_modules=ext_modules)
